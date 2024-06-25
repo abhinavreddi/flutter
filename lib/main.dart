f3c6bdf46d10ef8238eb6356a5d4bb73294e95d0
ABHINAV REDDY <abhinavnalasani@gmail.com>
2024-06-25 17:43:20 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:flutter_application_1/Pages/login_page.dart';
+
+import 'Pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => Homepage(),
+        "/login": (context) => Homepage(),
+      },
     );
   }
 }
